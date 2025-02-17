1f2140ee761f807e3171202c165fd4ced1dd8284
NotMonika <l73583@126.com>
2025-02-17 19:05:36 +0800

    打包一个全平台的javafx

---
+++ javafx/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+}
+
+group = "rip.sayori.rmcr"
+version = "11"
+
+repositories {
+    maven("https://maven.aliyun.com/repository/public")
+}
+
+val deps = arrayOf("base","controls","graphics","media","swing","web")
+val platforms = arrayOf("win","linux") //mac dog都不用
+
+dependencies {
+    deps.forEach { name ->
+        platforms.forEach { plat ->
+            implementation("org.openjfx:javafx-$name:$version:$plat")
+        }
+    }
+}
+
+tasks.jar{
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    configurations.runtimeClasspath.get().forEach {
+        if(it.isDirectory())
+            from(it)
+        else
+            from(zipTree(it))
+    }
+}
